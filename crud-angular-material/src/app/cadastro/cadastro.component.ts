ccc5c3491ba6285521745946119c0f5b6c18b3b2
Maycon Bruno <114522993+MayconBruno7@users.noreply.github.com>
2026-05-28 06:45:11 -0300

    feature: criando um formulário.

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FlexLayoutModule, 
+    MatCardModule, 
+    FormsModule, 
+    MatInputModule
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
