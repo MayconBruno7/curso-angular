252fbad494852751e9f889922f9e36adbcf037af
Maycon Bruno <114522993+MayconBruno7@users.noreply.github.com>
2026-05-30 18:28:10 -0300

    feature: tela de consulta.

--- crud-angular-material/src/app/cliente.service.ts
+++ crud-angular-material/src/app/cliente.service.ts
@@ -2,13 +2,46 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cadastro/cliente';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
+  static REPO_CLIENTES = '_CLIENTES';
 
-  constructor() { }
+  constructor() {}
 
   salvar(cliente: Cliente) {
-    console.log("Salvando cliente: ", cliente);
+    const storage = this.obterStorage();
+
+    storage.push(cliente);
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage));
+  }
+
+  pesquisarClientes(nomeBusca: string): Cliente[] {
+    const clientes = this.obterStorage();
+    if (!nomeBusca) {
+      return clientes;
+    }
+
+    return clientes.filter((cliente) =>
+      cliente.nome?.toLowerCase().includes(nomeBusca.toLowerCase()),
+    );
+  }
+
+  private obterStorage(): Cliente[] {
+    const respositorioClientes = localStorage.getItem(
+      ClienteService.REPO_CLIENTES,
+    );
+
+    if (respositorioClientes) {
+      const clientes: Cliente[] = JSON.parse(respositorioClientes);
+      return clientes;
+    }
+
+    const clientes: Cliente[] = [];
+    localStorage.setItem(
+      ClienteService.REPO_CLIENTES,
+      JSON.stringify(clientes || '[]'),
+    );
+    return clientes;
   }
 }
